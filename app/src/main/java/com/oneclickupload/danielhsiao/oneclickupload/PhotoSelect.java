9b766c600732370d56ce5bd3e330789fcaf37b4c
daniel0514 <daniel0514@gmail.com>
2017-01-03 15:52:31 -0800

    implemented photo selection screen

--- app/src/main/java/com/oneclickupload/danielhsiao/oneclickupload/PhotoSelect.java
+++ app/src/main/java/com/oneclickupload/danielhsiao/oneclickupload/PhotoSelect.java
@@ -2,6 +2,10 @@ package com.oneclickupload.danielhsiao.oneclickupload;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 public class PhotoSelect extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class PhotoSelect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_select);
+
+        GridView photoGrid = (GridView) findViewById(R.id.gridview);
+        photoGrid.setAdapter(new ImageAdapter(this));
+
+        photoGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(PhotoSelect.this, "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
