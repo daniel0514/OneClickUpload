c11140a547e08cc12320f1c8b962ef9b8ef45a85
daniel0514 <daniel0514@gmail.com>
2017-01-03 21:10:35 -0800

    Implemented Permission Request and reading camera roll file locations

--- app/src/main/java/com/oneclickupload/danielhsiao/oneclickupload/ImageAdapter.java
+++ app/src/main/java/com/oneclickupload/danielhsiao/oneclickupload/ImageAdapter.java
@@ -1,11 +1,17 @@
 package com.oneclickupload.danielhsiao.oneclickupload;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.os.Environment;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Daniel Hsiao on 2017-01-03.
@@ -13,9 +19,17 @@ import android.widget.ImageView;
 
 public class ImageAdapter extends BaseAdapter {
     private Context mContext;
+    private List<String> images;
+    private static final String CAMERA_IMAGE_BUCKET_NAME =
+            Environment.getExternalStorageDirectory().toString()
+                    + "/DCIM/Camera";
+    private static final String CAMERA_IMAGE_BUCKET_ID =
+            getBucketId(CAMERA_IMAGE_BUCKET_NAME);
+
 
     public ImageAdapter(Context c){
         mContext = c;
+        images = getCameraImages(mContext);
     }
 
     @Override
@@ -51,6 +65,30 @@ public class ImageAdapter extends BaseAdapter {
 
     }
 
+    private static String getBucketId(String path) {
+        return String.valueOf(path.toLowerCase().hashCode());
+    }
+
+    private static List<String> getCameraImages(Context context) {
+        final String[] projection = { MediaStore.Images.Media.DATA };
+        final String selection = MediaStore.Images.Media.BUCKET_ID + " = ?";
+        final String[] selectionArgs = { CAMERA_IMAGE_BUCKET_ID };
+        final Cursor cursor = context.getContentResolver().query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                projection,
+                selection,
+                selectionArgs,
+                null);
+        ArrayList<String> result = new ArrayList<>(cursor.getCount());
+        if (cursor.moveToFirst()) {
+            final int dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            do {
+                final String data = cursor.getString(dataColumn);
+                result.add(data);
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+        return result;
+    }
     // references to our images
     private Integer[] mThumbIds = {
             R.drawable.sample_2, R.drawable.sample_3,
--- app/src/main/java/com/oneclickupload/danielhsiao/oneclickupload/PhotoSelect.java
+++ app/src/main/java/com/oneclickupload/danielhsiao/oneclickupload/PhotoSelect.java
@@ -1,5 +1,9 @@
 package com.oneclickupload.danielhsiao.oneclickupload;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,11 +12,18 @@ import android.widget.GridView;
 import android.widget.Toast;
 
 public class PhotoSelect extends AppCompatActivity {
-
+    int permissionCheck;
+    private static final int requestResult = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photo_select);
+
+        permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if(permissionCheck != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, requestResult);
+        }
+        permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
 
         GridView photoGrid = (GridView) findViewById(R.id.gridview);
         photoGrid.setAdapter(new ImageAdapter(this));
@@ -24,4 +35,18 @@ public class PhotoSelect extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch(requestCode){
+            case requestResult: {
+               if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(PhotoSelect.this, "Permission Granted", Toast.LENGTH_SHORT).show();
+               } else {
+                   Toast.makeText(PhotoSelect.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+               }
+            }
+        }
+    }
+
 }
